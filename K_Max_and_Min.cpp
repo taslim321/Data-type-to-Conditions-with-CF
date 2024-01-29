9d3f12cd48f1c53c492306478a9e43e3c46f44d3
Taslim Amin <121754467+taslim321@users.noreply.github.com>
2024-01-29 21:54:57 +0600

    Max Min

---
+++ K_Max_and_Min.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+// min_v = min(input, min_v);
+    // max_v = max(input, max_v);
+int main(){
+   int a,b,c;
+   cin>>a>>b>>c;
+   int min,max;
+   //max value
+   if(a>=b && a>=c)
+   {
+    max=a;
+   }else if(b>=a && b>=c)
+   {
+    max=b;
+   }else{
+    max=c;
+   }
+   //min value
+   if(a<=b && a<=c)
+   {
+    min=a;
+   }else if(b<=a && b<=c)
+   {
+    min=b;
+   }else{
+    min=c;
+   }
+   cout<<min<<" "<<max;
+    return 0;
+}
